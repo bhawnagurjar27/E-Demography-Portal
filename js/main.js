e0dbf5df11cacb5a0309015eb377599a2f724b7a
bhawnagurjar27 <bhawna111gurjar@gmail.com>
2021-04-11 09:38:56 +0530

    initial

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.img-slide').slick({
+               autoplay:true,
+               slideToShow:1,
+            
+               speed:800,
+               arrow:false,
+               dots:false,
+               fade:true, 
+               initial:true,
+               loop:true,
+    });
+});
